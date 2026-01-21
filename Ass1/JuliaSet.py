13fe3737f23153390595561157aa4ae9fbf55e4b
Pavel Urvantsev <pavelu@kth.se>
2026-01-21 02:08:20 +0100

    task 1 ish done

--- Ass1/JuliaSet.py
+++ Ass1/JuliaSet.py
@@ -17,6 +17,7 @@ def timefn(fn):
         return result
     return measure_time
 
+@profile
 def calc_pure_python(desired_width, max_iterations):
     """Create a list of complex coordinates (zs) and complex parameters (cs),
     build Julia set"""
@@ -53,8 +54,9 @@ def calc_pure_python(desired_width, max_iterations):
 
     # This sum is expected for a 1000^2 grid with 300 iterations
     # It ensures that our code evolves exactly as we'd intended
-    assert sum(output) == 33219980
+    #assert sum(output) == 33219980
 
+@profile
 def calculate_z_serial_purepython(maxiter, zs, cs):
     """Calculate output list using Julia update rule"""
     output = [0] * len(zs)
